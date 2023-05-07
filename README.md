841ad74026497ee6dacbe925414a7b2c86ee3239
Katharine McClenaghan <kemcclen@gmail.com>
2023-05-07 16:21:35 -0400

    generateMarkdown file completed with functions to fill the README and render the licence badge

---
+++ README.md
@@ -0,0 +1,32 @@
+ # fake title
+  
+## Description: 
+   good
+
+## Table of Contents: 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+   
+## Installation: 
+   download
+  
+## Usage: 
+   use it
+
+ ## License:
+The project is under the license: MIT 
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+## Contributions: 
+   dont
+   
+## Testing: 
+   trust me
+   
+## Contact: 
+   abcd@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return ` ## License:
+The project is under the license: ${license} `;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` # ${data.title}
+  
+## Description: 
+   ${data.description}
 
-`;
+## Table of Contents: 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+   
+## Installation: 
+   ${data.installation}
+  
+## Usage: 
+   ${data.usage}
+
+${renderLicenseSection(data.license)}
+
+${renderLicenseBadge(data.license)}
+  
+## Contributions: 
+   ${data.contribution}
+   
+## Testing: 
+   ${data.test}
+   
+## Contact: 
+   ${data.questions}`;
 }
 
 module.exports = generateMarkdown;
